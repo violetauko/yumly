bc5d29acff45e46012d0ca356fbc64b950fe4ea3
joe <joshua.okello@tridenstechnology.com>
2025-02-05 14:25:14 +0300

    created search bar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,25 +1,60 @@
-import { View, ScrollView, Image } from 'react-native'
-import React from 'react'
-import { StatusBar } from 'expo-status-bar'
-import {BellIcon} from 'react-native-heroicons/outline'
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { View, ScrollView, Image, Text, TextInput } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { BellIcon } from "react-native-heroicons/outline";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 const HomeScreen = () => {
   return (
-    <View className='flex-1 bg-white'>
-      <StatusBar style='dark'/>
-      <ScrollView 
-      showsVerticalScrollIndicator={false}
-      contentContainerStyle={{paddingBottom:50}}
-      className="space-y-6 pt-14"
+    <View className="flex-1 bg-white">
+      <StatusBar style="dark" />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 50 }}
+        className="space-y-6 pt-14"
       >
-        <View className='mx-4 flex-row justify-between items-center mb-2'>
-          <Image source={require('../../assets/images/profile.png')} style={{height:hp(5), width: hp(5)}} />
-          <BellIcon size={hp(4)} color="grey"/>
+        <View className="mx-4 flex-row justify-between items-center mb-2">
+          <Image
+            source={require("../../assets/images/profile.png")}
+            style={{ height: hp(5), width: hp(5) }}
+          />
+          <BellIcon size={hp(4)} color="grey" />
+        </View>
+
+        <View className="mx-4 space-y-2 mb-2">
+          <Text className="text-neutral-600" style={{ fontSize: hp(1.7) }}>
+            Hello there!
+          </Text>
+          <View>
+            <Text
+              className="text-neutral-600 font-semibold"
+              style={{ fontSize: hp(3.8) }}
+            >
+              Make your own food,
+            </Text>
+          </View>
+          <Text
+            className="text-neutral-600 font-semibold"
+            style={{ fontSize: hp(3.8) }}
+          >
+            stay at <Text className="text-amber-400">home</Text>
+          </Text>
+        </View>
+
+        <View className="mx-4 flex-row items-center rounded-full bg-black/5 p-[6px]">
+          <TextInput 
+          placeholder="Search recipes"
+          placeholderTextColor={'gray'}
+          style={{fontSize: hp(1.7)}}
+          className="flex-1 text-base mb-1 pl-3 tracking-wider"
+           />
         </View>
       </ScrollView>
     </View>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { View, Text, Image } from 'react-native'
 import React, { useEffect } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { StatusBar } from 'expo-status-bar'
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import Animated, { useSharedValue, withSpring } from 'react-native-reanimated';
 import { useRouter } from 'expo-router';
 
@@ -12,41 +12,41 @@ const index = () => {
     const ringPadding2 = useSharedValue(0);
 
     const router = useRouter();
-  
-    useEffect(()=>{
-        ringPadding1.value= 0
-        ringPadding2.value=0
-      setTimeout(()=> ringPadding1.value = withSpring(ringPadding1.value+hp(5)), 100);
-      setTimeout(()=> ringPadding2.value = withSpring(ringPadding2.value+hp(5.5)), 300);
 
-      const timeout = setTimeout(() => {
-        router.push("/(tabs)"); // Navigate to the home screen
-      }, 3000); // 3-second delay
-  
-      return () => clearTimeout(timeout);
-    },[])
+    useEffect(() => {
+        ringPadding1.value = 0
+        ringPadding2.value = 0
+        setTimeout(() => ringPadding1.value = withSpring(ringPadding1.value + hp(5)), 100);
+        setTimeout(() => ringPadding2.value = withSpring(ringPadding2.value + hp(5.5)), 300);
 
-    
+        const timeout = setTimeout(() => {
+            router.push("/(tabs)"); // Navigate to the home screen
+        }, 3000); // 3-second delay
 
-  return (
-    <SafeAreaView className='flex-1 justify-center items-center space-y-10 bg-amber-600'>
-        <StatusBar style='light'/>
+        return () => clearTimeout(timeout);
+    }, [])
 
-        <Animated.View className='bg-white/20 rounded-full' style={{padding:ringPadding1}}>
-        <Animated.View className='bg-white/20 rounded-full' style={{padding:ringPadding2}}>
-          <Image source={require('../assets/images/img1.png')}
-            style={{width:hp(20), height: hp(20)}}
-        />
-        </Animated.View>
-        </Animated.View>
-        <View className='flex items-center space-y-2 mt-5'>
-            <Text className='font-bold text-white/80 tracking-widest' style={{fontSize:hp(6)}}>Yumly</Text>
-            <Text className='font-medium text-white/45 tracking-widest' style={{fontSize:hp(2)}}>Your next meal awaits</Text>
-                  
-        </View>
-     
-    </SafeAreaView>
-  )
+
+
+    return (
+        <SafeAreaView className='flex-1 justify-center items-center space-y-10 bg-amber-600'>
+            <StatusBar style='light' />
+
+            <Animated.View className='bg-white/20 rounded-full' style={{ padding: ringPadding1 }}>
+                <Animated.View className='bg-white/20 rounded-full' style={{ padding: ringPadding2 }}>
+                    <Image source={require('../assets/images/img1.png')}
+                        style={{ width: hp(20), height: hp(20) }}
+                    />
+                </Animated.View>
+            </Animated.View>
+            <View className='flex items-center space-y-2 mt-5'>
+                <Text className='font-bold text-white/80 tracking-widest' style={{ fontSize: hp(6) }}>Yumly</Text>
+                <Text className='font-medium text-white/45 tracking-widest' style={{ fontSize: hp(2) }}>Your next meal awaits</Text>
+
+            </View>
+
+        </SafeAreaView>
+    )
 }
 
 export default index
