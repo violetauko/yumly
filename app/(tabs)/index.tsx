2995cf80d6356b07ef7f69f149e5436b4ba1b955
joe <joshua.okello@tridenstechnology.com>
2025-02-05 14:03:28 +0300

    created home screen top view

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,25 +1,25 @@
-// import { View, ScrollView, Image } from 'react-native'
-// import React from 'react'
-// import { StatusBar } from 'expo-status-bar'
-// import {BellIcon} from 'react-native-heroicons/outline'
-// import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { View, ScrollView, Image } from 'react-native'
+import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+import {BellIcon} from 'react-native-heroicons/outline'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
-// const HomeScreen = () => {
-//   return (
-//     <View className='flex-1 bg-white'>
-//       <StatusBar style='dark'/>
-//       <ScrollView 
-//       showsVerticalScrollIndicator={false}
-//       contentContainerStyle={{paddingBottom:50}}
-//       className="space-y-6 pt-14"
-//       >
-//         <View className='mx-4 flex-row justify-between items-center mb-2'>
-//           <Image source={require('../../assets/images/profile.png')} style={{height:hp(5), width: hp(5)}} />
-//           <BellIcon size={hp(4)} color="grey"/>
-//         </View>
-//       </ScrollView>
-//     </View>
-//   )
-// }
+const HomeScreen = () => {
+  return (
+    <View className='flex-1 bg-white'>
+      <StatusBar style='dark'/>
+      <ScrollView 
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={{paddingBottom:50}}
+      className="space-y-6 pt-14"
+      >
+        <View className='mx-4 flex-row justify-between items-center mb-2'>
+          <Image source={require('../../assets/images/profile.png')} style={{height:hp(5), width: hp(5)}} />
+          <BellIcon size={hp(4)} color="grey"/>
+        </View>
+      </ScrollView>
+    </View>
+  )
+}
 
-// export default HomeScreen
+export default HomeScreen
