e6614ad4b7162e1b3e356f4bd14353259481bdb9
joe <joshua.okello@tridenstechnology.com>
2025-02-06 14:50:17 +0300

    a change on MasonryList

--- components/Recipes.tsx
+++ components/Recipes.tsx
@@ -11,42 +11,33 @@ interface MealData {
     id: string;
     image: string;
 }
-
-
 const Recipes = () => {
     return (
         <View className='mx-4 space-y-3'>
             <Text style={{ fontSize: hp(3) }} className='font-semibold text-neutral-600'>Recipes</Text>
             <View>
-            <MasonryList
-  data={mealData}
-  keyExtractor={(item) => (item as MealData).id}
-  numColumns={2}
-  showsVerticalScrollIndicator={false}
-  renderItem={({
-    item,
-    i,
-  }: {
-    item: unknown; // MasonryList might give unknown
-    i: number;
-  }) => {
-    const mealItem = item as MealData;
-    return <RecipeCard item={mealItem} index={i} />;
-  }}
-  onEndReachedThreshold={0.1}
-/>
+                <MasonryList
+                    data={mealData}
+                    keyExtractor={(item) => item.id}
+                    numColumns={2}
+                    showsVerticalScrollIndicator={false}
+                    renderItem={({ item, i }) => (
+                        <RecipeCard item={item as MealData} index={i} />
+                    )}
+                    onEndReachedThreshold={0.1}
+                />
             </View>
         </View>
     )
 }
 
-const RecipeCard = ({ item, index, }: { item: { image: string }; index: number; }): JSX.Element => {
+export const RecipeCard = ({ item, index, }: { item: { image: string }; index: number; }): JSX.Element => {
     return (
         <View>
-            <Pressable style={{ width: '100%' }} className="flex justify-center mb-4 space-y-1">
+            <Pressable style={{ width: '100%' }} className="flex-1 justify-center mb-4 space-y-1">
                 <Image
                     source={{ uri: item.image }}
-                    style={{ width: '100%' }}
+                    style={{ width: '100%', height: hp(25), borderRadius: 35 }}
                     className="bg-black/5"
                 />
             </Pressable>
